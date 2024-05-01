1b481e2e721058df10db0e9e427af43663939e4f
saitogo555 <sg.saitogo@gmail.com>
2024-05-01 18:08:32 +0900

    Change default mode to dark

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,6 +58,9 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark'
+    },
     navbar: {
       title: 'STEMASK Unity Docs',
       logo: {
