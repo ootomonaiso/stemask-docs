f23b87ced5a72c88483b9159e56d82bcc97124e6
saitogo555 <sg.saitogo@gmail>
2024-05-02 23:27:56 +0900

    Change social card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/stemask-docs-social-card.jpg',
     colorMode: {
       defaultMode: 'dark'
     },
